b8a58a090481a458f01ed33af614e85becfe37ea
Christian Marc Bartolé <barch584@school.lu>
2026-04-30 10:15:26 +0200

    aaa

--- Test1/hw.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function changethis1(){
-document.getElementById("changethis").innerHTML = "Hello world uwu!";
-});
-/*window.addEventListener("load", function changethis() {
-    document.getElementById("changethis").innerHTML = "Hello world uwu!";
-})*/
